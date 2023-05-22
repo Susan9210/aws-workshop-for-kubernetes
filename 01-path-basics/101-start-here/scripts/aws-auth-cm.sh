449d4bbef42ecf4f4e2fbcc832f04f21c68b98cd
Susan V <susanv@Susans-MacBook-Air.local>
2023-05-22 15:19:16 -0500

    first commit

--- 01-path-basics/101-start-here/scripts/aws-auth-cm.sh
+++ 01-path-basics/101-start-here/scripts/aws-auth-cm.sh
@@ -6,9 +6,9 @@
 #date            2018-06-05
 #version         0.1
 #usage           curl -sSL https://s3.amazonaws.com/aws-kubernetes-artifacts/v0.5/aws-auth-cm.sh | bash -s stable
-#==============================================================================
+##==============================================================================
 
 curl -O https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/aws-auth-cm.yaml
 export EKS_WORKER_ROLE=$(aws cloudformation describe-stacks --stack-name k8s-workshop-worker-nodes | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="NodeInstanceRole")|.OutputValue')
 sed -i -e "s#<ARN of instance role (not instance profile)>#${EKS_WORKER_ROLE}#g" aws-auth-cm.yaml
-kubectl apply -f aws-auth-cm.yaml
+kubectl apply -f aws-auth-cm.yaml.
